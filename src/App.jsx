b1d7d9fc47a8d3d972d1a4b625e51373bcd63e83
Prathamesh9997 <gundep09@gmail.com>
2024-09-30 19:08:17 +0530

    PSG | add env

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import { ChakraProvider, Box } from "@chakra-ui/react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import AppRoutes from "./routes/AppRoutes";
 
-const GOOGLE_CLIENT_ID =
-  "561334803370-3jntct9vdq9e1rp9f6vbdo306t12na4q.apps.googleusercontent.com";
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 function App() {
   return (
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={clientId}>
       <ChakraProvider>
         <Box minH="100vh" bg="gray.50">
           <AppRoutes />
